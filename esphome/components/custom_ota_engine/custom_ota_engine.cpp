661db503269c89aad6b108c21cfda2db9a56d901
egelden <150428674+egelden@users.noreply.github.com>
2026-04-10 17:19:44 +0200

    Update custom_ota_engine.cpp

--- esphome/components/custom_ota_engine/custom_ota_engine.cpp
+++ esphome/components/custom_ota_engine/custom_ota_engine.cpp
@@ -2,6 +2,10 @@
 #include "esp_ota_ops.h"
 #include "esp_http_client.h"
 
+void CustomOtaEngine::setup() {
+  ESP_LOGI("custom_ota", "Custom OTA engine initialized");
+}
+
 void CustomOtaEngine::start_update() {
   if (firmware_url.empty()) {
     ESP_LOGE("custom_ota", "Geen firmware URL beschikbaar");
@@ -11,7 +15,7 @@ void CustomOtaEngine::start_update() {
   updating = true;
   ESP_LOGI("custom_ota", "OTA update gestart vanaf URL: %s", firmware_url.c_str());
 
-  // OTA-implementatie komt hier later
+  // OTA implementatie komt hier later
 }
 
 void CustomOtaEngine::loop() {
