d1e8df502a3ef6f2638b3065cf3665f96d738a15
egelden <150428674+egelden@users.noreply.github.com>
2026-04-10 17:00:11 +0200

    Create custom_ota_engine.h

---
+++ esphome/components/custom_ota_engine/custom_ota_engine.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "esphome.h"
+
+class CustomOtaEngine : public esphome::Component {
+ public:
+  std::string firmware_url;
+  bool updating = false;
+
+  void set_url(const std::string &url) {
+    firmware_url = url;
+  }
+
+  void start_update();
+
+  void loop() override;
+};
